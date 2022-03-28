51d9f86a5aea50b783c76e47ff5c027df9c37864
Martin Blomberg <martin.blomberg@outlook.com>
2022-03-28 11:24:06 +0200

    frange

---
+++ quiz.py
@@ -0,0 +1,13 @@
+def frange(start, stop, step):
+    if step == 0:
+        raise ValueError
+    while( start < stop):
+        yield start
+        start = start + step
+
+for x in frange(0.5, 5.5, 0.5):
+ print(x)
+
+
+
+
